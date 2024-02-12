a6fcf8392583f5872d69a73e68cd3547ba8995d2
Mahmudul Hasan Motin <152786481+mahmudulhasanmotin@users.noreply.github.com>
2024-02-12 14:40:50 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 
+
+const btn1 = document.getElementById('ratting-star1');
+
+btn1.addEventListener('click', function onClick(event) {
+  const box = document.getElementById('ratting-star1');
+
+  box.style.color = 'blue';
+
+});
+
+const btn2 = document.getElementById('ratting-star2');
+
+btn2.addEventListener('click', function onClick(event) {
+  const box = document.getElementById('ratting-star2');
+
+  box.style.color = 'blue';
+
+});
+const btn3 = document.getElementById('ratting-star3');
+
+btn3.addEventListener('click', function onClick(event) {
+  const box = document.getElementById('ratting-star3');
+
+  box.style.color = 'blue';
+
+});
+
+const btn4 = document.getElementById('ratting-star4');
+
+btn4.addEventListener('click', function onClick(event) {
+  const box = document.getElementById('ratting-star4');
+
+  box.style.color = 'blue';
+
+});
+
+const btn5 = document.getElementById('ratting-star5');
+
+btn5.addEventListener('click', function onClick(event) {
+  const box = document.getElementById('ratting-star5');
+
+  box.style.color = 'blue';
+
+});
+
+const btn6 = document.getElementById('ratting-star6');
+
+btn6.addEventListener('click', function onClick(event) {
+  const box = document.getElementById('ratting-star6');
+
+  box.style.color = 'blue';
+
+});
+
